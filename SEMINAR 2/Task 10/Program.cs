cd2680b9829b77ca015fe724bd7666c603392a80
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2022-09-09 22:24:01 +0300

    add Task 13

--- SEMINAR 2/Task 10/Program.cs
+++ SEMINAR 2/Task 10/Program.cs
@@ -1,5 +1,12 @@
-﻿Console.Clear();
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа "+stringNumber[1]);
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введи трёхзначное число: ");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("вторая цифра этого числа " + stringNumber[1]);
+    }
+}
---
+++ SEMINAR 2/Task 10/Task 13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int anyNumb = Convert.ToInt32(Console.ReadLine());
+string anyNumbText = Convert.ToString(anyNumb);
+if (anyNumbText.Length > 2){
+  Console.WriteLine(" третья цифра - " + anyNumbText[2]);
+}
+else {
+  Console.WriteLine(" третьей цифры нет ");
+}
