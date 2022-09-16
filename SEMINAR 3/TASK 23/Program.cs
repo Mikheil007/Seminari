1d74f3f45532d314f435235d6d4bdc710d5d2ff0
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-17 01:07:31 +0300

    add homework task 23

---
+++ SEMINAR 3/TASK 23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+//кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.Write($"{numN} -> ");
+while (numN >= i)
+{
+    Console.Write($"{i*i*i} ");
+    i = i + 1;
+}
