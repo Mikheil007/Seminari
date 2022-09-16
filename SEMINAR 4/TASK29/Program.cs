f60642e3fa41ed4c199159df4830aedafef7c407
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-17 00:04:50 +0300

    add homework task 29

---
+++ SEMINAR 4/TASK29/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int enterNum;
+Console.WriteLine("Будет создан массив длиной A в диапвзоне 0-A");
+Console.Write("Введите любое число (A): "); enterNum = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[enterNum];
+for (int i = 0; i < enterNum; i++)
+{
+array[i] = new Random().Next(enterNum + 1);
+Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
