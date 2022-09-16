b664cb0e64e167823b8dd091ced958af56051eff
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-17 01:51:36 +0300

    add homework task 38

---
+++ SEMINAR 5/TASK 38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("введите число, которому будет соответсвовать количество элементов в масиве ");
+int size = Convert.ToInt32(Console.ReadLine());
+double [] myarray = new double [6];
+double max = myarray[0];
+double min = myarray[0];
+for (int i = 0; i < myarray.Length; i++)
+{
+   myarray[i] = new Random().NextDouble();
+   max = myarray.Max();
+   min = myarray.Min();
+   }
+Console.WriteLine( $"[{string.Join(", ", myarray)}]");
+Console.WriteLine();
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine();
+Console.WriteLine($"разница между максимальным и минимальным значение: {max - min} ");
