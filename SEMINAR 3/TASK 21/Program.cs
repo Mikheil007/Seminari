a8671b7136ed206f0ba811f3160f7469bdb207d6
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-17 01:04:21 +0300

    add homework task 21

---
+++ SEMINAR 3/TASK 21/Program.cs
@@ -0,0 +1,18 @@
+﻿// 21 Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координты точки A");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координты точки A");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координты точки А");
+int x3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координты точки B");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координты точки B");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координты точки B");
+int y3 = Convert.ToInt32(Console.ReadLine());
+double w;
+w = Math.Sqrt(Math.Pow(x1-y1,2)+Math.Pow(x2-y2,2)+Math.Pow(x3-y3,2));
+Console.WriteLine($" расстояние между координат {w} ");
