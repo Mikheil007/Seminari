547cc54b84dd224d969d2aee95dd6f367d142bcf
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-16 23:44:58 +0300

    add homework task 25

---
+++ SEMINAR 4/TASK25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+int enterNumA, enterNumB;
+int result;
+Console.WriteLine("Для формулы A^B");
+Console.Write("Введите число A: ");
+enterNumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+enterNumB = Convert.ToInt32(Console.ReadLine());
+result = enterNumA;
+for (int i = 2; i <= enterNumB; i++)
+{
+result *= enterNumA;
+}
+Console.WriteLine($"(число А возведённая в натуральную степень В {enterNumA}^{enterNumB} = {result}");
