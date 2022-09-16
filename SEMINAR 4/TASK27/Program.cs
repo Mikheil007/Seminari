f44e183d73c3d9509deb8ff6d3af9fc06646ad66
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-16 23:57:45 +0300

    add homework task 27

---
+++ SEMINAR 4/TASK27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+int result;
+int enterNum;
+Console.Write("Введите любое число: ");
+enterNum = Convert.ToInt32(Console.ReadLine());
+result = 0;
+while (enterNum != 0)
+{
+result += enterNum % 10;
+enterNum /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {result}");
